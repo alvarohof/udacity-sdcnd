699d39689c139740d88e0957584ceff59c0678ae
alvarohof <alvarohof@yahoo.es>
2017-02-19 21:23:04 +0000

    Initial commit 'Finding line lanes project' code

---
+++ finding line lanes/t1-quiz1-quiz-color-selection.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 19 14:20:00 2017
+
+@author: alvaroho
+
+SDC ND - Term 1
+Project 1: Finding Lane Lines
+Quiz 1: color selection
+
+"""
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+# Read in the image
+image = mpimg.imread('test.jpg')
+
+# Grab the x and y size and make a copy of the image
+ysize = image.shape[0]
+xsize = image.shape[1]
+color_select = np.copy(image)
+
+# Define color selection criteria
+###### MODIFY THESE VARIABLES TO MAKE YOUR COLOR SELECTION
+red_threshold = 0
+green_threshold = 228
+blue_threshold = 0
+######
+
+rgb_threshold = [red_threshold, green_threshold, blue_threshold]
+
+# Do a boolean or with the "|" character to identify
+# pixels below the thresholds
+thresholds = (image[:,:,0] < rgb_threshold[0]) \
+            | (image[:,:,1] < rgb_threshold[1]) \
+            | (image[:,:,2] < rgb_threshold[2])
+color_select[thresholds] = [0,0,0]
+plt.imshow(color_select)
+# Display the image                 
+plt.imshow(color_select)
+
+# Uncomment the following code if you are running the code locally and wish to save the image
+mpimg.imsave("test-after.jpg", color_select)
